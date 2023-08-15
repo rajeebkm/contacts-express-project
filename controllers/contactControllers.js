35c015cc0df4c44be8991c21ac41ff53db0a99b9
Rajeeb Kumar Malik <rkm2541997@gmail.com>
2023-08-15 13:39:28 +0530

    controllers added for req, response

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -1,1 +1,57 @@
 // Controllers contains all Logics for request, response, connect to database, etc
+// Whenever we create the API methods, we always need to give some labels to that
+
+
+const getContacts = (req, res) => {
+    res.status(200).json({
+        message: "Get all contacts",
+    });
+}
+
+//@desc Get all contact
+//route GET /api/contacts/:id
+//@access public
+const getContact = (req, res) => {
+  // res.send("Hello World!");
+  res.status(200).json({
+    message: `Get contact for ${req.params.id}`,
+  });
+};
+
+//@desc Create New contacts
+//route POST /api/contacts (201: resource created)
+//@access public
+const createContact = (req, res) => {
+  // res.send("Hello World!");
+  res.status(201).json({
+    message: "Create contacts",
+  });
+}
+
+//@desc Update contact
+//route POST /api/contacts/:id
+//@access public
+const updateContact = (req, res) => {
+  // res.send("Hello World!");
+  res.status(200).json({
+    message: `Update contact for ${req.params.id}`,
+  });
+};
+
+//@desc Delete contact
+//route POST /api/contacts
+//@access public
+const deleteContact = (req, res) => {
+  // res.send("Hello World!");
+  res.status(200).json({
+    message: `Delete contact for ${req.params.id}`,
+  });
+};
+
+module.exports = {
+    getContacts,
+    getContact,
+    createContact,
+    updateContact,
+    deleteContact,
+}
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,40 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { getContacts, getContact, createContact, updateContact, deleteContact } = require("../controllers/contactControllers");
 
 
-router.route("/").get((req, res) => {
-  // res.send("Hello World!");
-  res.status(200).json({
-    message: "Get all contacts",
-  });
-});
+// router.route("/").get((req, res) => {
+//   // res.send("Hello World!");
+//   res.status(200).json({
+//     message: "Get all contacts",
+//   });
+// });
 
-router.route("/:id").get((req, res) => {
-  // res.send("Hello World!");
-  res.status(200).json({
-    message: `Get contact for ${req.params.id}`,
-  });
-});
+// router.route("/").get(getContacts);
 
-router.route("/").post((req, res) => {
-  // res.send("Hello World!");
-  res.status(200).json({
-    message: "Create contacts",
-  });
-});
+// router.route("/:id").get(getContact);
+router.route("/").get(getContacts).post(createContact);
 
-router.route("/:id").put((req, res) => {
-  // res.send("Hello World!");
-  res.status(200).json({
-    message: `Update contacts for ${req.params.id}`,
-  });
-});
+// router.route("/").post(createContact);
 
-router.route("/:id").delete((req, res) => {
-  // res.send("Hello World!");
-  res.status(200).json({
-    message: `Delete contacts for ${req.params.id}`,
-  });
-});
+// router.route("/:id").put(updateContact);
+
+// router.route("/:id").delete(deleteContact);
+
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 module.exports = router;
